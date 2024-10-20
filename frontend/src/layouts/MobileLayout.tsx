3f63d9a57816b698f5d74805558f673c57a23af8
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-10-20 22:01:48 +0200

    fixed error in header mount

--- frontend/src/layouts/MobileLayout.tsx
+++ frontend/src/layouts/MobileLayout.tsx
@@ -1,5 +1,4 @@
-// src/layouts/MobileLayout.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import MobileHeader from '@components/MobileHeader';
 import Footer from '@components/Footer';
 
@@ -8,9 +7,11 @@ interface MobileLayoutProps {
 }
 
 const MobileLayout: React.FC<MobileLayoutProps> = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="flex flex-col min-h-screen">
-      <MobileHeader />
+      <MobileHeader isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <main className="flex-grow container mx-auto px-4 py-8">
         {children}
       </main>
